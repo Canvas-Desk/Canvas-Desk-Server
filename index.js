b9d8ef6f0b8b0baaa44d90a872b23327cee16491
Sashank Thupukari <sthupukari@gmail.com>
2015-02-07 02:44:45 -0500

    Set up socket.io and express

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ var app = require('express')();
 
 var http = require('http').Server(app);
 
+io.on('connection', function(socket) {
+    console.log("connected");
+
+    socket.on("disconnect", function() {
+        console.log("disconnect");
+    });
+});
+
+console.log("Started");
+
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
