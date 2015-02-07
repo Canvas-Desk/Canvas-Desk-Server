0987ca1e5055623402d702d723345f429a636607
Sashank Thupukari <sthupukari@gmail.com>
2015-02-07 02:46:09 -0500

    Express default route and basic websocket events

--- index.js
+++ index.js
@@ -3,17 +3,47 @@ var app = require('express')();
 
 var http = require('http').Server(app);
 
+var settings = {
+    brushSize: 5.01,
+    color: [185.0, 55.0, 100.0],
+    colorChoose: {
+        primary: "#111",
+        secondary: "#222"
+    },
+    currentLayer: "_id",
+    layers: {
+        //list
+    },
+    currentTool: "brush"
+}
+
+
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
 io.on('connection', function(socket) {
     console.log("connected");
+
+    io.on("changeBrushSize", function(newSize) {
+        settings.brushSize = newSize;
+        io.emit("updateBrushSize", settings.brushSize)
+    });
+
+    io.on("changeTool", function(newTool){
+    	settings.currentTool = newTool;
+    	io.emit("updateTool", settings.currentTool)
+    });    
 
     socket.on("disconnect", function() {
         console.log("disconnect");
     });
 });
 
-console.log("Started");
+console.log("Socket.io listening on *:8080");
 
 
 http.listen(3000, function() {
-    console.log('listening on *:3000');
+    console.log('Express listening on *:3000');
 });
